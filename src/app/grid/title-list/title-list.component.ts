93015e79c0fbf8dcade66ac48eeff7301e6f051b
stephengeorgewest <stephen.george.west@gmail.com>
2022-03-16 11:39:30 -0600

    group by start time

--- src/app/grid/title-list/title-list.component.ts
+++ src/app/grid/title-list/title-list.component.ts
@@ -3,10 +3,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GridEntry } from 'src/app/api/epg/events/grid/responsemodel';
 import { ignoreEntry, IgnoreListService, listNames } from 'src/app/ignore-list.service';
 
-type t = "past" | "now" | "next" | "tomorrow" | "nextWeek" | "nextMonth";
-type t2 = Exclude<t, "past" | "next">;
+type coarseTimeGroupKeys = "past" | "now" | "next" | "tomorrow" | "nextWeek" | "nextMonth";
+type dateBoundryKeys = Exclude<coarseTimeGroupKeys, "past" | "next">;
 
-function getDates(): { [key in t2]: number } {
+type title = string;
+type start = number
+
+function getDates(): { [key in dateBoundryKeys]: number } {
 	const now = Date.now() / 1000;
 
 	const todayDate = new Date();
@@ -33,47 +36,62 @@ function getDates(): { [key in t2]: number } {
 })
 export class TitleListComponent {
 	public times = getDates();
-	public grouped: {
-		[key in t]: Map<string, GridEntry[]>
+	public coarseTimeGroups: {
+		[key in coarseTimeGroupKeys]: Map<start, Map<title, GridEntry[]>>
 	} = {
-			past: new Map<string, GridEntry[]>(),
-			now: new Map<string, GridEntry[]>(),
-			next: new Map<string, GridEntry[]>(),
-			tomorrow: new Map<string, GridEntry[]>(),
-			nextWeek: new Map<string, GridEntry[]>(),
-			nextMonth: new Map<string, GridEntry[]>()
+			past: new Map<start, Map<title, GridEntry[]>>(),
+			now: new Map<start, Map<title, GridEntry[]>>(),
+			next: new Map<start, Map<title, GridEntry[]>>(),
+			tomorrow: new Map<start, Map<title, GridEntry[]>>(),
+			nextWeek: new Map<start, Map<title, GridEntry[]>>(),
+			nextMonth: new Map<start, Map<title, GridEntry[]>>()
 		};
 
-	@Input() public set filteredEntries(filteredEntries: Map<string, GridEntry[]>) {
-		this.grouped = [...filteredEntries.entries()].reduce((prev, [key, value]) => {
-			if (value[0].stop < this.times.now) {
-				prev.past.set(key, value);
-			}
-			else if (value[0].start <= this.times.now) {
-				prev.now.set(key, value);
-			}
-			else if (this.times.now <= value[0].start && value[0].start < this.times.tomorrow) {
-				prev.next.set(key, value);
-			}
-			else if (this.times.tomorrow <= value[0].start && value[0].start < this.times.nextWeek) {
-				prev.tomorrow.set(key, value);
-			}
-			else if (this.times.nextWeek <= value[0].start && value[0].start < this.times.nextMonth) {
-				prev.nextWeek.set(key, value);
-			}
-			else {
-				prev.nextMonth.set(key, value);
-			}
-			return prev;
-		}, {
-			past: new Map<string, GridEntry[]>(),
-			now: new Map<string, GridEntry[]>(),
-			next: new Map<string, GridEntry[]>(),
-			tomorrow: new Map<string, GridEntry[]>(),
-			nextWeek: new Map<string, GridEntry[]>(),
-			nextMonth: new Map<string, GridEntry[]>()
-		});
+	@Input() public set filteredEntries(filteredEntries: Map<title, GridEntry[]>) {
+		this.times = getDates();
+		this._filteredEntries = filteredEntries;
+		this.reGroup();
 	}
+	private _filteredEntries: Map<title, GridEntry[]> | undefined = undefined;
+	private reGroup() {
+		if (this._filteredEntries) {
+			this.coarseTimeGroups = [...this._filteredEntries.entries()].reduce((prev, [title, entryList]) => {
+				let coarseTimeGroup = prev.nextMonth;
+				if (entryList[0].stop < this.times.now) {
+					coarseTimeGroup = prev.past;
+				}
+				else if (entryList[0].start <= this.times.now) {
+					coarseTimeGroup = prev.now;
+				}
+				else if (this.times.now <= entryList[0].start && entryList[0].start < this.times.tomorrow) {
+					coarseTimeGroup = prev.next;
+				}
+				else if (this.times.tomorrow <= entryList[0].start && entryList[0].start < this.times.nextWeek) {
+					coarseTimeGroup = prev.tomorrow;
+				}
+				else if (this.times.nextWeek <= entryList[0].start && entryList[0].start < this.times.nextMonth) {
+					coarseTimeGroup = prev.nextWeek;
+				}
+
+				const fineTimeGroup = coarseTimeGroup.get(entryList[0].start);
+				if(fineTimeGroup) {
+					fineTimeGroup.set(title, entryList)
+				}
+				else {
+					coarseTimeGroup.set(entryList[0].start, new Map([[title, entryList]]))
+				}
+				return prev;
+			}, {
+				past: new Map<start, Map<title, GridEntry[]>>(),
+				now: new Map<start, Map<title, GridEntry[]>>(),
+				next: new Map<start, Map<title, GridEntry[]>>(),
+				tomorrow: new Map<start, Map<title, GridEntry[]>>(),
+				nextWeek: new Map<start, Map<title, GridEntry[]>>(),
+				nextMonth: new Map<start, Map<title, GridEntry[]>>()
+			});
+		}
+	}
+
 	@Input() public lastignoredcount: number = 0;
 	@Output() public selectedEntry: EventEmitter<GridEntry[]> = new EventEmitter();
 
@@ -109,14 +127,14 @@ export class TitleListComponent {
 	public ignoreChanelName(channelName: string, listName: listNames) {
 		this.ignoreService.ignoreChanelName(channelName, listName);
 	}
-	
-	public groupOrder(a: KeyValue<t, Map<string, GridEntry[]>>, b: KeyValue<t, Map<string, GridEntry[]>>) {
-		const keySortOrder: t[] = [
+
+	public coarseTimeGroupOrder(a: KeyValue<coarseTimeGroupKeys,any>, b: KeyValue<coarseTimeGroupKeys, any>) {
+		const keySortOrder: coarseTimeGroupKeys[] = [
 			"past", "now", "next", "tomorrow", "nextWeek", "nextMonth"
 		];
 		return keySortOrder.indexOf(a.key) - keySortOrder.indexOf(b.key);
 	}
-	public otherOrder(a: KeyValue<string, GridEntry[]>, b: KeyValue<string, GridEntry[]>) {
+	public fineTimeGroupOrder(a: KeyValue<string, GridEntry[]>, b: KeyValue<string, GridEntry[]>) {
 		if (a.value[0].start === b.value[0].start) {
 			const channel_a = a.value[0].channelNumber.split(".");
 			const channel_b = b.value[0].channelNumber.split(".");
