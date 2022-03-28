f8e20e2626a1eb5d89f806ac7280b6ac58586667
stephengeorgewest <stephen.george.west@gmail.com>
2022-03-28 00:03:26 -0600

    add hover names to navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { FinishedComponent } from './dvr/finished/finished.component';
 import { EpgComponent } from './epg/epg.component';
 import { IgnoreListManagmentComponent } from './ignore-list-managment/ignore-list-managment.component';
 export const routes: Routes = [
+	// TODO: https://dev.to/brandontroberts/setting-page-titles-natively-with-the-angular-router-393j
 	{ path: "grid", component: EpgComponent, data: { icon: "view_list", friendlyName: "Grid" } },
 	{ path: "upcoming_recording", component: UpcomingComponent, data: {  icon: "videocam", friendlyName: "Upcoming Recordings"} },
 	{ path: "finished_recording", component: FinishedComponent, data: {  icon: "save", friendlyName: "Finished Recordings"} },
