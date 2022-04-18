49a216fb0d556a45b07073e2e10d45b97a304097
stephengeorgewest <stephen.george.west@gmail.com>
2022-04-18 10:56:46 -0600

    better scrolling entry list

--- src/app/epg/entry-list/entry-list.component.ts
+++ src/app/epg/entry-list/entry-list.component.ts
@@ -17,14 +17,26 @@ export class EntryListComponent implements OnChanges {
 	constructor(private apiService: ApiService, public dialog: MatDialog) { }
 	ngOnChanges(changes: SimpleChanges): void {
 		if (!changes['selectedEntry'].firstChange)
-			this.select(this.selectedEntry[0])
+			this.select(this.selectedEntry[0], true);
 	}
-	public click(event: Event, entry: GridEntry) {
-		event.stopPropagation();
-		this.select(entry);
-	}
-	private select(entry: GridEntry | undefined) {
-			this.topEntry = entry;
+	public select(entry: GridEntry | undefined, scroll?: boolean) {
+		this.topEntry = entry;
+		if (scroll) {
+			const container = document.getElementById("entry-list");
+			if (container) {
+				const id = entry?.eventId || 0;
+				const target = document.getElementById(id.toString());
+				if (!target) {
+					container?.scrollTo({ behavior: "smooth", top: 0 });
+				}
+				else if (target.getBoundingClientRect().bottom > container.getBoundingClientRect().bottom) {
+					target.scrollIntoView({ behavior: 'smooth', block: "end" });
+				}
+				else if (target.getBoundingClientRect().top < container.getBoundingClientRect().top) {
+					target.scrollIntoView({ behavior: 'smooth', block: "start" });
+				}
+			}
+		}
 	}
 	public open() {
 		this.dialog.open(EntryDialog, {
@@ -33,14 +45,36 @@ export class EntryListComponent implements OnChanges {
 		});
 	}
 	public pendingAPI = false;
-	public record(event_id: number){
+	public record(event_id: number) {
 		this.pendingAPI = true;
-		this.apiService.createByEvent({event_id, config_uuid: ""}).catch(() => this.pendingAPI = false).then(() => this.pendingAPI = false);
+		this.apiService.createByEvent({ event_id, config_uuid: "" }).catch(() => this.pendingAPI = false).then(() => this.pendingAPI = false);
 	}
-	public stop(entry: GridEntry){
+	public stop(entry: GridEntry) {
 		this.pendingAPI = true;
 		this.dialog.open(ConfirmStopDialog, {
 			data: entry
 		}).afterClosed().subscribe(() => this.pendingAPI = false);
+	}
+	public prev() {
+		let entry;
+		if (!this.topEntry)
+			entry = this.selectedEntry[0];
+		else {
+			const index = this.selectedEntry.findIndex(e => e.eventId === this.topEntry?.eventId);
+			if (index !== 0)
+				entry = this.selectedEntry[index - 1];
+		}
+		this.select(entry, true);
+	}
+	public next() {
+		let entry;
+		if (!this.topEntry)
+			entry = this.selectedEntry[0];
+		else {
+			const index = this.selectedEntry.findIndex(e => e.eventId === this.topEntry?.eventId);
+			if (index !== this.selectedEntry.length - 1)
+				entry = this.selectedEntry[index + 1];
+		}
+		this.select(entry, true);
 	}
 }
