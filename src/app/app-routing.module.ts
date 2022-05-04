7c355b4d2b122d609e065579135c2fcae382db4c
Stephen West <stephen.george.west@gmail.com>
2022-05-04 05:54:01 -0600

    router file formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './auth.guard';
+
 import { AboutComponent } from './about/about.component';
 import { UpcomingComponent } from './dvr/upcoming/upcoming.component';
 import { FinishedComponent } from './dvr/finished/finished.component';
 import { EpgComponent } from './epg/epg.component';
 import { IgnoreListManagmentComponent } from './ignore-list-managment/ignore-list-managment.component';
-import { AuthGuard } from './auth.guard';
 import { InputComponent } from './status/input/input.component';
 export const routes: Routes = [
-	{ path: "grid", component: EpgComponent, data: { icon: "view_list", friendlyName: "Grid" } },
-	{ path: "upcoming_recording", component: UpcomingComponent, data: {  icon: "videocam", friendlyName: "Upcoming Recordings", guard: "dvr"}, canActivate:[AuthGuard] },
-	{ path: "finished_recording", component: FinishedComponent, data: {  icon: "save", friendlyName: "Finished Recordings", guard: "dvr"}, canActivate:[AuthGuard] },
-	{ path: "stream", component: InputComponent, data: {  icon: "insights", friendlyName: "Streams", guard: "admin"}, canActivate:[AuthGuard] },
-	{ path: "ignore", component: IgnoreListManagmentComponent, data: { icon: "thumb_down", friendlyName: "Ignore List" } },
-	{ path: "about", component: AboutComponent, data: { icon: "info", friendlyName: "About" } },
-
+	{
+		path: "grid",
+		component: EpgComponent,
+		data: { icon: "view_list", friendlyName: "Grid" }
+	},
+	{
+		path: "upcoming_recording",
+		component: UpcomingComponent,
+		data: { icon: "videocam", friendlyName: "Upcoming Recordings", guard: "dvr" },
+		canActivate: [AuthGuard]
+	},
+	{
+		path: "finished_recording",
+		component: FinishedComponent,
+		data: { icon: "save", friendlyName: "Finished Recordings", guard: "dvr" },
+		canActivate: [AuthGuard]
+	},
+	{
+		path: "stream",
+		component: InputComponent,
+		data: { icon: "insights", friendlyName: "Streams", guard: "admin" },
+		canActivate: [AuthGuard]
+	},
+	{
+		path: "ignore",
+		component: IgnoreListManagmentComponent,
+		data: { icon: "thumb_down", friendlyName: "Ignore List" }
+	},
+	{
+		path: "about",
+		component: AboutComponent,
+		data: { icon: "info", friendlyName: "About" }
+	},
 ];
 
 @NgModule({
